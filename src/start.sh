a1cfd45c9c7d368085852abf54795e36d1f129a6
blackbeans.zc <blackbeans.zc@gmail.com>
2013-03-27 21:12:15 +0800

    改名

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-go build meishi.go
+go build weixin.go
 ps uax | grep weixin | awk 'NR==FNR{system("kill "$2) }'
 
 sleep 10
 
-nohup ./meishi >> stdout.log 2>&1 &
+nohup ./weixin >> stdout.log 2>&1 &
